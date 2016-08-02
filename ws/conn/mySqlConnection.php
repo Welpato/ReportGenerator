8704b28bb236e774d85e3f7de104512d31d714d2
Wesley Osorio <wesleyfosorio@hotmail.com>
2016-08-02 15:55:14 -0300

    Added the mysql connection functions

--- ws/conn/mySqlConnection.php
+++ ws/conn/mySqlConnection.php
@@ -3,6 +3,7 @@ class connect{
 	private $SERVER="";
 	private $USER="";
 	private $PASSWORD="";
+	private $DBNAME="";
 	private $connection;
 	public $error;
 	function __construct(){
@@ -10,12 +11,51 @@ class connect{
 	}
 
 	function connect(){
+		$this->connection = new mysqli($this->SERVER, $this->USER, $this->PASSWORD,$this->DBNAME) ;
+		if($this->connection->connect_error){
+			$this->error = "Error on DB connect!";
+		}
 	}
 	
 	function executeSearch($sql){
+		$this->connect();
+		
+		if(isset($this->error)){
+			return false;
+		}
+		if(!isset($sql) || $sql == ''){
+			$this->erro = "SQL command not informed!";
+			return false;
+		}
+		$search = $this->connection->query($sql);
+		$arrReturn = array();
+		$i = 0;
+		if($search->num_rows > 0){
+			while ($row = $search->fetch_assoc()) {
+				$arrReturn[$i] = $row;
+				$i++;
+			}
+			
+		}
+		$this->connection->close();
+		return $arrReturn;
 	}
 	
 	function executeCommand($sql){
+		$this->connect();
+		if(isset($this->erro)){			
+			return false;
+		}
+		if(!isset($sql) || $sql == ''){
+			$this->error = "SQL command not informed!";
+			return false;
+		}
+		$resp = $this->connection->query($sql);
+		$this->connection->close();
+		return $resp;
+		
+		return false;
+		
 	}
 }
 
